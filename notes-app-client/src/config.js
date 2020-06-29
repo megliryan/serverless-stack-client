361d21df6fab67fb9aa0a904c9e9f369f610ce29
Ryan Megli <10722474@uvu.edu>
2020-06-29 13:00:24 -0600

    Setting up our React app

---
+++ notes-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-creation"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://dvph0lq3x6.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_3JuOQV6Va",
+    APP_CLIENT_ID: "40cg485t9htfo398mprps8i72j",
+    IDENTITY_POOL_ID: "us-east-1:9f3cacda-4f1c-47c1-baa6-d078ccc3e466"
+  }
+};
